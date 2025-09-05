eab92294849388edc93b3a2c73736cab9ffc9791
gizag889 <gizaguri0426@outlook.jp>
2025-09-05 19:36:47 +0900

    レビュー一覧コンポーネントの作成

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import Footer from "./components/Footer";
+import ReviewList from "./components/ReviewList";
+ 
+const SummarySection = () => {
+  return (
+    <div className="w-2/5 h-full overflow-hidden pl-20">
+      {/* <ReviewSummary /> */}
+    </div>
+  );
+};
+ 
+const ReviewListSection = () => {
+  return (
+    <div className="w-3/5 h-full overflow-y-auto pr-20">
+      <ReviewList />
+    </div>
+  );
+};
+ 
+function App() {
+  return (
+    <>
+      <div className="h-[calc(100vh-68px)] flex gap-12 pt-12">
+        <SummarySection />
+        <ReviewListSection />
+      </div>
+      <Footer />
+    </>
+  );
+}
+ 
+export default App;
